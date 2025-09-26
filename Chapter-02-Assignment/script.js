6fe9a674b8b95bca8bf919ce5314b218815e48c5
harmainsajid <harmainsajid23@gmail.com>
2025-09-26 17:40:21 +0500

    Assignments Completed

---
+++ Chapter-02-Assignment/script.js
@@ -0,0 +1,36 @@
+// Q1
+let userName;
+
+// Q2
+let myName = "Harmain Sajid";
+
+// Q3
+let message = "Hello world!";
+alert(message);
+
+// Q4
+let name = "Jhon Doe";
+let age = 15;
+let degree = "Certified Mobile Application Development";
+alert(name);
+alert(age + " years old");
+alert(degree);
+
+// Q5
+let food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(food);
+
+// Q6
+let email = "harmainsajid23@gmail.com";
+alert("My email address is " + email);
+
+// Q7
+let book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+// Q8
+document.writeln("Yah! I can write HTML content through JavaScript");
+
+// Q9
+let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
---
+++ Chapter-03-Assignment/script.js
@@ -0,0 +1,34 @@
+// Q1
+let age = 20;
+alert("I am " + age + " years old");
+
+// Q2
+let visits = 14;
+alert("You have visited this site for " + visits + " times");
+
+// Q3
+let birthYear = 2005;
+document.writeln(
+  "My birth year is " +
+    birthYear +
+    " <br/> Data type of my declared variable is number <br/>"
+);
+
+// Q4
+let visitorName = "Jhon Doe";
+let productTitle = "T-Shirt(s)";
+let quantity = 5;
+document.writeln(
+  "<b>" +
+    visitorName +
+    "</b>" +
+    " ordered " +
+    "<b>" +
+    quantity +
+    "</b>" +
+    " " +
+    "<b>" +
+    productTitle +
+    "</b>" +
+    " from XYZ company."
+);
